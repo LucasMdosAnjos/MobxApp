d09de87df7616a69cc9be550adf0674e3c88c303
LucasMdosAnjos <lucasmon@live.com>
2020-01-06 20:16:11 -0300

    mobx com computed properties

--- lib/controller.dart
+++ lib/controller.dart
@@ -4,12 +4,21 @@ part 'controller.g.dart';
 class Controller = ControllerBase with _$Controller;
 
 abstract class ControllerBase with Store{
+  @observable
+  String nome = '';
 
   @observable
-  int counter = 0;
+  String sobrenome = '';
+  @computed
+  String get nomecompleto => "$nome $sobrenome";
 
   @action
-  increment(){
-    counter++;
+  mudarNome(String newName){
+    nome = newName;
   }
+  @action
+  mudarSobreNome(String newName){
+    sobrenome = newName;
+  }
+
 }
--- lib/home.dart
+++ lib/home.dart
@@ -10,27 +10,40 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
         title: Text('Mobx'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Observer(
-              builder: (_){
-                return Text(
-                  '${controller.counter}',
-                  style: Theme.of(context).textTheme.display1,
-                );
-              },
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(labelText: 'Nome'),
+                onChanged: controller.mudarNome,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Sobrenome'),
+                onChanged: controller.mudarSobreNome,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Observer(
+                builder: (_){
+                  return Text('${controller.nomecompleto}');
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          controller.increment();
+        onPressed: () {
+//          controller.increment();
         },
         tooltip: 'Increment',
         child: Icon(Icons.add),
