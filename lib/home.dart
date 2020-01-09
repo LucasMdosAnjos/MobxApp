aae3e232162cf50e3b10107f1c8beb5d35fd12e3
LucasMdosAnjos <lucasmon@live.com>
2020-01-09 16:08:41 -0300

    alterado provider para get_it.

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx_app/controller.dart';
 
 import 'body.dart';
-import 'controller.dart';
 
 class MyHomePage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    final controller = Provider.of<Controller>(context);
+    final controller = GetIt.I.get<Controller>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Observer(builder:(_){return Text(controller.isValid ? 'Formulário Validado' : "Formulário Não Validado");}),
-      ),
-      body: BodyWidget()
-    );
+        appBar: AppBar(
+          title: Observer(builder: (_) {
+            return Text(controller.isValid
+                ? 'Formulário Validado'
+                : "Formulário Não Validado");
+          }),
+        ),
+        body: BodyWidget());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:get_it/get_it.dart';
 
 import 'controller.dart';
 import 'home.dart';
 
-void main() => runApp(MyApp());
+void main() {
+
+  GetIt getIt = GetIt.I;
+  getIt.registerSingleton<Controller>(Controller());
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<Controller>(
-          create: (_) => Controller(),
-          dispose: (_, controller)=> controller.dispose(),
-        ),
-      ],
-      child: MaterialApp(
-        title: 'Flutter MobX',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: MyHomePage(),
+    return MaterialApp(
+      title: 'Flutter MobX',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
     );
   }
 }
